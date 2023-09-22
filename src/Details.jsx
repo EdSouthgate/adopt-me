2c051cc6afaaef2e6e7d33b0382ba8ebf3ff0b7c
Edmund Southgate <edsouthgate@me.com>
2023-09-22 12:04:39 +0100

    adds portal usage

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,41 +1,54 @@
 import ErrorBoundary from "./ErrorBoundary";
-import {useParams} from "react-router-dom";
-import {useQuery} from "@tanstack/react-query";
+import Modal from "./Modal";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import Carousel from "./Carousel";
 import fetchPet from "./fetchPet.js";
 const Details = () => {
-    const {id} = useParams();
-    const results = useQuery(["details", id], fetchPet);
+  const [showModal, setShowModal] = useState(false);
+  const { id } = useParams();
+  const results = useQuery(["details", id], fetchPet);
 
-    if(results.isLoading) {
-        return (
-            <div className="loading-pane">
-            <h2 className="loader">🤸🏼</h2>
-            </div>
-        );
-    }
-
-    const pet = results.data.pets[0]
+  if (results.isLoading) {
     return (
-        <div className="details">
-            <Carousel images={pet.images} />
-            <div> 
-                <h1>{pet.name}</h1>
-                <h2>{`${pet.animal} - ${pet.breed} - ${pet.city}, ${pet.state}`}</h2>
-                <button>Adopt {pet.name}</button>
-                <p>{pet.description}</p>
-            </div>
-        </div>
-    )
-}
+      <div className="loading-pane">
+        <h2 className="loader">🤸🏼</h2>
+      </div>
+    );
+  }
 
+  const pet = results.data.pets[0];
+  return (
+    <div className="details">
+      <Carousel images={pet.images} />
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>{`${pet.animal} - ${pet.breed} - ${pet.city}, ${pet.state}`}</h2>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {pet.name}?</h1>
+              <div className="buttons">
+                <button>Yes</button>
+                <button onClick={() => setShowModal(false)}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
+      </div>
+    </div>
+  );
+};
 
 function DetailsErrorBoundary(props) {
-    return (
-        <ErrorBoundary>
-            <Details {...props} />
-        </ErrorBoundary>
-    );
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
 }
 
 export default DetailsErrorBoundary;
